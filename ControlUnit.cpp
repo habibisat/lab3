7a9aa2ffb0797ae2af316ded844cdeb4e2a30060
habibisat <99595729+habibisat@users.noreply.github.com>
2022-03-13 21:10:58 +0600

    Add files via upload

---
+++ ControlUnit.cpp
@@ -0,0 +1,168 @@
+// Verilator generated module include.
+#include "VControlUnit.h"
+
+// C++ libraries.
+#include "stdlib.h"
+#include <iostream>
+
+// Verilator libraries.
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+using dut_type = VControlUnit;
+
+uint32_t global_time = 0;
+
+struct input_package {
+    // input bits
+    unsigned int clk;
+    unsigned int Resetn = 0;
+
+
+    // 9-bit input busses
+    unsigned int IR : 9;
+};
+
+struct output_package {
+    //unsigned int unlck;
+    unsigned int a = 0;
+    unsigned int g = 0;
+    unsigned int done = 0;
+    unsigned int alu_op = 0;
+    unsigned int mux : 3;
+    unsigned int registers : 8;
+};
+
+static void advance_sim(dut_type* top, VerilatedVcdC* trace)
+{
+    trace->dump(global_time);
+    top->eval();
+    ++global_time;
+}
+    // CMD = 000 is move command
+    // CMD = 001 is move immediate command
+    // CMD = 010 is add command
+    // CMD = 011 is substract command
+
+int main() {
+    srand(1234);
+
+    dut_type* top = new dut_type;
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* trace = new VerilatedVcdC;
+    top->trace(trace, 99);
+    trace->open("ControlUnit.vcd");
+
+    input_package inp;
+    output_package out;
+
+    inp.IR = 0b010101110; 
+    
+    int CMD = (inp.IR & 0b111000000)/64;
+    int X_Address = (inp.IR & 0b000111000)/8;
+    int Y_Address = inp.IR & 0b000000111;
+
+    int iter = 20;
+    int CurrentState = 0;
+    int NextState = 0;
+
+    while(iter > 0)
+    {
+        inp.clk = global_time % 2;
+        top->clk = inp.clk;
+        switch (inp.clk)
+       
+       {
+       case 1:
+           if (inp.Resetn || out.done){
+               CurrentState = 0;
+           } else {
+               CurrentState = NextState;
+           }
+           break;
+       case 0:
+            top->IR = inp.IR;
+            break;
+       }
+
+        top->Resetn = inp.Resetn;
+
+        switch (CurrentState)
+        {
+        case 0:
+            out.a = 0;
+            out.g = 0;
+            out.registers = 0;
+            out.mux = 0;
+            out.alu_op = 0;
+            out.done = 0;
+            if (CMD == 0b010 || CMD ==0b011){NextState = 1;}            
+            //else if (inp.ir == ((inp.ir & 0b000111111)|0b011000000) || inp.ir == ((inp.ir & 0b000111111)|0b010000000)) {NextState = 1;}
+            //else if (inp.ir == inp.ir & 0b001111111) {NextState = 5;}
+            else if(CMD == 0b000){NextState = 4;}
+            else if(CMD == 0b001){NextState = 5;} 
+            //else {NextState = 0;}
+            break;
+        case 1:
+            out.mux = 0b0000 + X_Address+1;
+            out.registers = 1*2^(X_Address);
+            out.a = 1;
+            NextState = 2;
+            out.done = 0;
+            break;
+        case 2:
+            out.mux = 0b0000 + Y_Address + 1;
+            out.registers = 1*2^(Y_Address);
+            if (inp.IR > 64)
+            {
+                out.alu_op = 1;
+            } else { out.alu_op = 0;}
+            out.a = 1;
+            out.g = 1;
+            NextState = 3;
+            out.done = 0;
+            break;
+        case 3:
+            out.a = 0;
+            out.alu_op = 0;
+            out.mux = 0b1111;
+            out.g = 1;
+            out.registers = 1*2^(X_Address);
+            out.done = 1;
+            NextState = 0;
+            break;
+        case 4:
+            out.mux = 0b0000 + X_Address +1;
+            out.registers = 1*2^(X_Address) + 1*2^(Y_Address);
+            out.done = 1;
+            break;            
+        case 5:
+            out.mux = 0b0000;
+            out.registers = 1*2^(X_Address);
+            out.done = 1;
+            break;    
+        default:
+            break;
+        }
+        
+        top->eval();
+        advance_sim(top, trace);
+
+        if(top->done != out.done) {
+            std::cout << "iter" << iter << "; ";
+            std::cout << "clk" << inp.clk << "; ";
+            std::cout << "instruction" << inp.IR << "; ";
+            std::cout << "registers = " << (int)out.registers << "; ";
+            std::cout << "a = " << (int)out.a << "; ";
+            std::cout << "g = " << (int)out.g << "; ";
+            std::cout << "alu_op = " << (int)out.alu_op << "; ";
+            std::cout << "mux = " << (int)out.mux << "; ";
+            std::cout << "done = " << (int)out.done << "; ";
+        }
+
+        //sim iteration
+        iter--;
+    }
+
+    trace->close();
+}
---
+++ ControlUnit.v
@@ -0,0 +1,125 @@
+/* verilator lint_off UNUSED */
+module ControlUnit(
+    
+    input wire [8:0] IR,
+    input wire Run,
+    input wire Resetn,
+    input wire clk,
+
+    output reg a,
+    output reg g,
+    output reg [3:0] mux,
+    output reg alu_op,
+    output reg [7:0] registers,
+    output reg done,
+    output reg IRen
+    );
+
+assign IRen = 1;
+
+localparam STATE_IDLE = 3'd0,
+    STATE_ALU_T1 = 3'd1, // alu_op = 0
+    STATE_ALU_T2 = 3'd2, 
+    STATE_ALU_T3 = 3'd3,
+    STATE_MV = 3'd4,
+    STATE_MVI = 3'd5;
+
+reg [2:0] CurrentState;
+reg [2:0] NextState;
+
+reg [2:0] CMD;
+reg [2:0] X_Address;
+reg [2:0] Y_Address;
+
+
+// -----------------------------------------------------------------------------
+// Conditional State - Transition
+// -----------------------------------------------------------------------------
+
+always@ ( posedge clk ) begin
+    if ( Resetn || done ) begin
+        CurrentState <= STATE_IDLE;
+        IRen <=1;
+    end
+    else begin 
+        CurrentState <= NextState;
+        IRen <= 0;
+    end
+end
+
+always@ ( * ) begin
+    CMD = IR[6+:3];
+    X_Address = IR[3+:3];
+    Y_Address = IR[0+:3];
+    NextState = CurrentState ;
+
+    // CMD = 000 is move command
+    // CMD = 001 is move immediate command
+    // CMD = 010 is add command
+    // CMD = 011 is substract command
+
+    case ( CurrentState )
+        STATE_IDLE : begin
+            a = 0;
+            g = 0;
+            registers = 8'd0;
+            alu_op = 0;
+            done = 0;
+            if (X_Address != 3'd0 && Y_Address != 3'd0) begin 
+                if (CMD == 3'b000) NextState = STATE_MV;
+                else if (CMD == 3'b011 || CMD == 3'b010) NextState = STATE_ALU_T1;
+                else if (CMD == 3'b001) NextState = STATE_MVI;
+            end
+        end
+
+        STATE_ALU_T1 : begin
+            mux = X_Address+1;
+            registers[X_Address] = 1;
+            a = 1;
+            NextState = STATE_ALU_T2;
+            done = 0;
+        end
+
+        STATE_ALU_T2 : begin
+            registers[X_Address] = 0;
+            mux = Y_Address + 1;
+            registers[Y_Address] = 1;
+            alu_op = CMD[0];
+            a = 1;
+            g = 1;
+            NextState = STATE_ALU_T3;
+            done = 0;
+        end
+
+        STATE_ALU_T3 : begin
+            a = 0;
+            registers[Y_Address] = 0;
+            alu_op = 0;
+            mux = 4'd9;
+            g = 1;
+            registers[X_Address] = 1;
+            done = 1;
+        end
+
+        STATE_MV : begin
+            mux = X_Address + 4'd1;
+            registers[X_Address] = 1;
+            registers[Y_Address] = 1;
+            done = 1;
+        end
+
+        STATE_MVI: begin
+            mux=4'd0;
+            registers[X_Address] = 1;
+            done = 1;
+        end
+
+        default : begin
+            NextState = STATE_IDLE;
+        end
+    endcase
+end
+// -----------------------------------------------------------------------------
+
+endmodule
+// -----------------------------------------------------------------------------
